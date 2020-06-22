7f5ba23bebdff570bc201f53520e57d96b974aba
Mackyson <mackyson72@gmail.com>
2020-06-22 17:46:56 +0900

    HTTPのエラーが出た瞬間に標準出力するのをやめて，呼び出し元へerror型で返す

--- SearchTwitter/handler/HTTPResponseCheck.go
+++ SearchTwitter/handler/HTTPResponseCheck.go
@@ -5,19 +5,18 @@ import (
 )
 
 func HTTPResponseCheck(statusCode int) error {
-	// var err error //TODO 標準出力脱却する
 	if statusCode != 200 {
-		fmt.Printf("正常に処理されませんでした エラーコード:%d\n", statusCode)
+		msg := fmt.Sprintf("正常に処理されませんでした エラーコード:%d\n", statusCode)
 		switch statusCode {
 		case 403:
-			fmt.Println("多分API Key，Secret，トークンが間違っています")
+			msg += fmt.Sprintf("多分API Key，Secret，トークンが間違っています\n")
 		case 404:
-			fmt.Println("URLのTypoやAPI側のリソースURLの変更を確認してください")
+			msg += fmt.Sprintf("URLのTypoやAPI側のリソースURLの変更を確認してください\n")
 		case 420, 429:
-			fmt.Printf("クエリ送りすぎです ")
+			msg += fmt.Sprintf("クエリ送りすぎです\n")
 		}
-		fmt.Println("https://developer.twitter.com/ja/docs/basics/response-codes で詳細を確認してください")
-		return fmt.Errorf("ErrorCode:%d", statusCode)
+		msg += ("https://developer.twitter.com/ja/docs/basics/response-codes で詳細を確認してください")
+		return fmt.Errorf(msg)
 	}
 	return nil
 }
