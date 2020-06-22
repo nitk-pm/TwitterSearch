17ef6f9a6d17a3d8b97d1376311dc755cc429a9a
Mackyson <mackyson72@gmail.com>
2020-06-22 17:33:21 +0900

    APIコール部分を完全に切り分け

---
+++ SearchTwitter/handler/searchTweets.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"../model"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+func SearchTweets(accessToken string, queryParam string, url string) (*model.SearchResponse, error) {
+	req, err := http.NewRequest("GET", url+"?q="+queryParam, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", "Bearer "+accessToken)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if err := HTTPResponseCheck(res.StatusCode); err != nil {
+		return nil, err
+	}
+	// fmt.Printf("%+v\n", res) //Search APIのレスポンス確認
+	defer res.Body.Close()
+	searchResponse := &model.SearchResponse{}
+	body, _ := ioutil.ReadAll(res.Body)
+	if err := json.Unmarshal(body, searchResponse); err != nil {
+		return nil, err
+	}
+	return searchResponse, nil
+}
--- SearchTwitter/main.go
+++ SearchTwitter/main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"./handler"
 	"./model"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"net/url"
 	"os"
 )
@@ -30,32 +27,10 @@ func main() {
 		log.Fatal(err)
 	}
 	queryParam := url.QueryEscape("from:@nosykcam")
-	req, _ := http.NewRequest("GET", resourceURL+"?q="+queryParam, nil)
-	req.Header.Add("Authorization", "Bearer "+accessToken)
+	searchResponse, err := handler.SearchTweets(accessToken, queryParam, resourceURL)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	//TODO res2とかいうバカみたいな変数名をどうにかしろ
-	res2, _ := http.DefaultClient.Do(req)
-	// fmt.Printf("%+v\n", res2) //Search APIのレスポンス確認
-	if res2.StatusCode != 200 {
-		fmt.Printf("正常に処理されませんでした エラーコード:%d\n", res2.StatusCode)
-		switch res2.StatusCode {
-		case 403:
-			fmt.Println("多分トークンが間違っています")
-		case 404:
-			fmt.Println("URLのTypoやAPI側のリソースURLの変更を確認してください")
-		case 406:
-			fmt.Println("検索クエリの中身に不正な値が含まれています")
-		case 420, 429:
-			fmt.Printf("クエリ送りすぎです ")
-		}
-		fmt.Println("https://developer.twitter.com/ja/docs/basics/response-codes で詳細を確認してください")
-		return
-	}
-	defer res2.Body.Close()
-	searchResponse := &model.SearchResponse{}
-	body, _ := ioutil.ReadAll(res2.Body)
-	if err := json.Unmarshal(body, searchResponse); err != nil {
-		fmt.Println("error:", err)
-	}
 	fmt.Println(searchResponse.Statuses[0].Text)
 }
