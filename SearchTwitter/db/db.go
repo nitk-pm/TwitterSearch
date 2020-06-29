8790bc17befc6a4c9b754f96cb502a08c2011638
Mackyson <mackyson72@gmail.com>
2020-06-29 23:22:53 +0900

    Elasticsearchにデータを投げる

--- SearchTwitter/db/db.go
+++ SearchTwitter/db/db.go
@@ -1,4 +1,30 @@
+// package db
 package main
 
+import (
+	"fmt"
+	"github.com/elastic/go-elasticsearch/v8"
+	// "github.com/elastic/go-elasticsearch/v8/esapi"
+	"log"
+	"strings"
+)
+
 func main() {
+	es, err := elasticsearch.NewDefaultClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+	text := "{\"title\":\"test3\"}"
+	res, err := AddData(es, text)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res)
+}
+func AddData(es *elasticsearch.Client, text string) (string, error) { //副作用あります，しょうがないよね．
+	res, err := es.Index(
+		"test", //Index(データベース)の名前
+		strings.NewReader("{\"title\":\"test2\"}"), //Document(レコード)はReader型でないとだめ(多分クソデカいデータ入れることもあるため)
+	) //"_type"は_docで固定，"_id"はデータ記録時にランダム生成
+	return res.String(), err
 }
---
+++ SearchTwitter/go.mod
@@ -0,0 +1,5 @@
+module github.com/elastic/elastic/go-elasticsearch/v7
+
+go 1.14
+
+require github.com/elastic/go-elasticsearch/v8 v8.0.0-20200625154855-2c4ce25bd45c // indirect
