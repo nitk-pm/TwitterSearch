b0174d42b1989a8be220d20e648ec91b186a29a3
Mackyson <mackyson72@gmail.com>
2020-07-06 21:12:10 +0900

    クレデンシャル作る関数をpublicにする必要性ゼロ

--- SearchTwitter/handler/getToken.go
+++ SearchTwitter/handler/getToken.go
@@ -2,18 +2,24 @@ package handler
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"repEATer/model"
 )
 
+func makeCredential(oauth model.OAuth) string {
+	credential := base64.URLEncoding.EncodeToString([]byte(oauth.ConsumerKey + ":" + oauth.ConsumerSecret))
+	return credential
+}
+
 func GetToken(oauth model.OAuth, url string) (string, error) {
 	req, err := http.NewRequest("POST", url, bytes.NewBufferString("grant_type=client_credentials"))
 	if err != nil {
 		return "", err
 	}
-	req.Header.Add("Authorization", "Basic "+MakeCredential(oauth))
+	req.Header.Add("Authorization", "Basic "+makeCredential(oauth))
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
 	res, err := http.DefaultClient.Do(req)
 	// fmt.Printf("%+v\n", res) //トークンリクエストのレスポンス確認
